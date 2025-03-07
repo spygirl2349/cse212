0af166fbb72655273cd4665128a9e06aab948fb3
spygirl2349 <spygirl2349@gmail.com>
2025-03-07 16:23:27 -0700

    week 1 complete

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +14,23 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // ***PLAN***
+        // 1. Create an array.
+        // 2. Fill the array with multiples of the first number passed in for the length decided by the 2nd
+        // number that is passed in.
+        // 3. Return the array.
+        // ***END PLAN***
+        double [] multiples = new double[length];
+        multiples[0] = number; 
+        int j = 0;
+        for (int i = 1; i < length; i++)
+        {
+            multiples[i] = multiples[j] + number; 
+            j++;
+        };
 
-        return []; // replace this return statement with your own
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +46,17 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // *** PLAN ***
+        // 1. Get the length of the list  
+        // 2. use getRange method to split data
+        // 2. Make 2 lists, and combine 2 lists into one in correct order.
+        int length = data.Count ;
+        List<int> list1 = data.GetRange(length - amount, amount);
+        List<int> list2 = data.GetRange(0, length - amount);
+
+        list1.AddRange(list2);
+
+        data.Clear();
+        data.AddRange(list1);
     }
 }
